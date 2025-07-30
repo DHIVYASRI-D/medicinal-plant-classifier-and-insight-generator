413492c928b08ac7367927cbbede237b288ffc6b
DHIVYASRI-D <dhivyasricse@gmail.com>
2025-07-30 11:51:46 +0530

    Delete tempCodeRunnerFile.py

--- train/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-from transformers import ViTImageProcessor
-
-processor = ViTImageProcessor.from_pretrained("google/vit-base-patch16-224-in21k")
-processor.save_pretrained("D:/medicinal-plant-classifier-and-insight-generator/outputs/vit-plant-classifier")
